d7ab77c809d08545c775cf3c3203b76ea63892cc
MónicaSC <monica.sccr@gmail.com>
2025-05-28 23:52:30 -0600

    Dios

--- FitLifeMoviles/FitLifeMoviles/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FitLifeMoviles.AssemblyInfo.cs
+++ FitLifeMoviles/FitLifeMoviles/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FitLifeMoviles.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitLifeMoviles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5f47725704ecdaefaf7bb3a981423a143b09eeb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c982ee6593eeb6f3f37485c8ab1a7e5943179eb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitLifeMoviles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitLifeMoviles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
